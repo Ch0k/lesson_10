7b615570a2572b832bbbc22a8d0bf62f9b2430bd
andreevsergey <andreev,sv89@yandex.ru>
2021-07-02 18:52:41 +0300

    add comment and new methods to main

--- main.rb
+++ main.rb
@@ -4,7 +4,25 @@ class BlackJack
     puts 'Введите имя игрока'
     x = get.chomp.to_s
     create_player(x)
+    # Игрок получает 2 карты
+    recive_card_to_player
+    # Диллер получает 2 карты
+    recive_card_to_diller
+    # Выводим количество очков игрока
+    puts player_points
+    # Отправляем деньги пользователя в банк
 
+    send_player_money_to_bank
+            # Отправляем деньги дилера в банк
+
+    send_player_money_to_bank
+    puts 'propustit'
+    puts 'add_card'
+    puts 'open_card'
+    y = get.chomp.to_s
+    case 
+    propustit_hod_diller
+    add_card_to_diller
   end
 
   def create_player(name)
@@ -13,6 +31,7 @@ class BlackJack
 
   def seed
     coloda = %w(Туз 2 3 4 5 6 7 8 9 10 Валет Дама Король)
+    Masti = 
     card1 = Card.new('2',2)
     card2 = Card.new('3',3)
     card3 = Card.new('4',4)
